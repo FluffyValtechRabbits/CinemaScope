12dfcb19e51f7b14e2fd2a69e4746693f8965f03
Viktor Zozuliuk <zozulyuk22@gmail.com>
2022-05-10 10:56:52 +0300

    Added Entities to MovieService

--- MovieService/Context/MovieContext.cs
+++ MovieService/Context/MovieContext.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MovieService.Context
 {
-    internal class MovieContext
+    public class MovieContext : DbContext
     {
+        
+
+        public MovieContext() : base("MovieDbContext") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ MovieService/Entities/Movie.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace MovieService.Entities
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(220)]
+        public string Title { get; set; }
+
+        public Bitmap Poster { get; set; }
+
+        [Required]
+        [Range(1895,2030)]
+        public int Year { get; set; }
+
+        [Required]
+        public int TypeId { get; set; }
+
+        public virtual Type Type { get; set; }
+
+        public string Cast { get; set; }
+
+        public string Plot { get; set; }
+
+        public int? Budget { get; set; }
+
+        public int? BoxOffice { get; set; }
+
+        public double? RatingIMDb { get; set; }
+
+        public double? SiteUsersRating { get; set; }
+
+        public virtual ICollection<Genre> Genres { get; set; }
+
+        public virtual ICollection<Country> Countries { get; set; }
+    }
+}
