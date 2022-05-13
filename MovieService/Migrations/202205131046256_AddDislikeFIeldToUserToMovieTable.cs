944041890ef0ffd8032431c2ad2bfb25d124887f
Dmytro Filiuk <dmytro.filiuk@valtech.com>
2022-05-13 13:48:50 +0300

    Added dislike field to UserToMovies model

---
+++ MovieService/Migrations/202205131046256_AddDislikeFIeldToUserToMovieTable.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDislikeFIeldToUserToMovieTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.UserToMovies", "IsDisLiked", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.UserToMovies", "IsDisLiked");
+        }
+    }
+}
