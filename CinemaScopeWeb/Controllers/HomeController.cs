e34f00359ae8225d53838f36414910241cf0cf99
fundowakin1 <kalchenko.yehor@lll.kpi.ua>
2022-05-18 18:45:49 +0300

    completed search feature

--- CinemaScopeWeb/Controllers/HomeController.cs
+++ CinemaScopeWeb/Controllers/HomeController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using CinemaScopeWeb.ViewModels;
+using Microsoft.Ajax.Utilities;
 using MovieService.Entities;
 using MovieService.Interfaces;
+using Unity.Injection;
 
 namespace CinemaScopeWeb.Controllers
 {
@@ -28,6 +31,25 @@ namespace CinemaScopeWeb.Controllers
                     Title = movie.Title
                 }).ToList();
             return View(moviesToView);
+        }
+
+        [HttpPost]
+        public ActionResult SearchResult(string input)
+        {
+            if (input.IsNullOrWhiteSpace())
+                return RedirectToAction("Index");
+            var moviesToView = _unitOfWork.MovieRepository.GetAll()
+                .Select(movie => new MovieToHomeViewModel()
+                {
+                    Id = movie.Id,
+                    Poster = movie.Poster,
+                    Title = movie.Title
+                }).ToList();
+            var inputRegex = new Regex($"(\\b{input.ToUpper()})|(\\b{input.ToUpper()}\\b)");
+            var movieWithFiltering = moviesToView
+                .Where(word => inputRegex.IsMatch(word.Title.ToUpper()))
+                .ToList();
+            return View("Index", movieWithFiltering);
         }
     }
 }
