6a44a10a2dc4c70b0b157512051a7a03b723f8f0
Viktor Zozuliuk <zozulyuk22@gmail.com>
2022-05-30 19:37:42 +0300

    Fixed the view of aboutUs

--- CinemaScopeWeb/ViewModels/AboutUs/CreateAboutUsViewModel.cs
+++ CinemaScopeWeb/ViewModels/AboutUs/CreateAboutUsViewModel.cs
@@ -8,7 +8,8 @@ namespace CinemaScopeWeb.ViewModels
         [Required(ErrorMessage = "Name is required.")]
         [Display(Name = "Name:")]
         [RegularExpression(@"^(?!.*\s\s)(?!.*\.\.)(?!.*,,)[A-Z][a-zA-Z .,]{2,60}$",
-            ErrorMessage = "Name is not correct.")]
+            ErrorMessage = "Names must consist of letters, " +
+                    "begin with main letter and can include \" - \" symbol.")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Description is required.")]
