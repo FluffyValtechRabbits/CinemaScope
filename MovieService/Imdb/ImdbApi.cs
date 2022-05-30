2db843cd6202fd5c6e17d9de0e2d6669cdd9b5fd
Dmytro Filiuk <dmytro.filiuk@valtech.com>
2022-05-30 17:32:52 +0300

    scheduler fixed

--- MovieService/Imdb/ImdbApi.cs
+++ MovieService/Imdb/ImdbApi.cs
@@ -2,13 +2,13 @@
 {
     public static class ImdbApi
     {
-        public static string apiKey = "k_p8yk7sg3";
+        public static string apiKey = "k_zqennz0k";
         public static string searchRequest = "https://imdb-api.com/en/API/Search/{0}/{1}";
         public static string movieRequest = "https://imdb-api.com/en/API/Title/{0}/{1}/FullActor,FullCast,Posters,Images,Ratings,";
         public static string top250Request = "https://imdb-api.com/en/API/Top250Movies/{0}";
         public static string MoiveIdCode = "tt";
         public static string MovieIdStartNumber = "0110413";
         public static int MaxMovieUpdateTries = 10;
-        public static int MaxMovieUpdates = 8;
+        public static int MaxMovieUpdates = 1;
     }
 }
